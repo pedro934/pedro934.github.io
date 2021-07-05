9c74e0fef4e78b91030f7d893a428a14c0bcd040
Pedro Luna <better.call.pedroluna@gmail.com>
2021-07-05 12:20:07 -0300

    Add the javascript to the code

---
+++ js/game.js
@@ -0,0 +1,52 @@
+//cache elements
+const d4 = document.getElementById('d4');
+const d6 = document.getElementById('d6');
+const d8 = document.getElementById('d8');
+const d10 = document.getElementById('d10');
+const d12 = document.getElementById('d12');
+const d20 = document.getElementById('d20');
+const result = document.getElementById('result');
+
+// dice function than rolls the dice
+function dice(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// main function than creates the events in each dice
+function main() {
+  d4.addEventListener('click', () => {
+    let r = dice(1, 4);
+    result.innerText = r;
+    console.log(r);
+  });
+  d6.addEventListener('click', () => {
+    let r = dice(1, 6);
+    result.innerText = r;
+    console.log(r);
+  });
+  d8.addEventListener('click', () => {
+    let r = dice(1, 8);
+    result.innerText = r;
+    console.log(r);
+  });
+  d10.addEventListener('click', () => {
+    let r = dice(1, 10);
+    result.innerText = r;
+    console.log(r);
+  });
+  d12.addEventListener('click', () => {
+    let r = dice(1, 12);
+    result.innerText = r;
+    console.log(r);
+  });
+  d20.addEventListener('click', () => {
+    let r = dice(1, 20);
+    result.innerText = r;
+    console.log(r);
+  });
+}
+
+// callback the main function
+main();
